a921b13d4f9108d143180c8733fca4c68864c2cb
Vu Xuan Tu <tuvu884884@gmail.com>
2022-04-01 13:46:52 +0700

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>32234</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
